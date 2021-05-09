e1b191bcafe466887775aa3f097baa665e16ede0
Bob (AlyonaB) <Vivaldition@yandex.ru>
2021-05-09 23:53:13 +0300

    code: add min(A,B)

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main()
     cout << "A + B = " << a + b << '\n'
      << "A - B = " << a - b << '\n'
      << "A * B = " << a * b << '\n'
-     << "A / B = " << a / b << '\n';
+     << "A / B = " << a / b << '\n'
+     << "Min = " << min(a,b) << '\n';
     return 0;
 }
